3df930aa263175d67ecfe4982bd779928dc1c531
Eli Babila <elibabila@gmail.com>
2023-03-23 18:19:02 +0200

    Added kitty

--- vim-setup.sh
+++ vim-setup.sh
@@ -7,3 +7,7 @@ ln -s ${HOME}/.dotfiles/vim/.gvimrc ${HOME}/.gvimrc
 # nvim
 ln -s ${HOME}/.dotfiles/neovim/init.vim ${HOME}/.config/nvim/init.vim
 ln -s ${HOME}/.dotfiles/neovim/ginit.vim ${HOME}/.config/nvim/ginit.vim
+
+# kitty
+ln -s ${HOME}/.dotfiles/kitty/kitty.conf ${HOME}/.config/kitty/kitty.conf
+ln -s ${HOME}/.dotfiles/kitty/pager ${HOME}/.config/kitty/pager
